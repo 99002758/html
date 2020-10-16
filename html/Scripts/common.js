7e76d8e7a273fae0326ae9e2672950a7d0f2ceb8
99002758 <72496047+99002758@users.noreply.github.com>
2020-10-16 16:41:56 +0530

    Add files via upload

---
+++ html/Scripts/common.js
@@ -0,0 +1,13 @@
+function $(id){
+	return document.getElementById(id);
+}
+
+
+function $hide(id){
+	$(id).style.display = "none"; 
+}
+
+function $show(id){
+	$(id).style.display = "inline-block";
+}
+
